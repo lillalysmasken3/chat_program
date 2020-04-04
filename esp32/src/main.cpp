4c9d1753e8cfb832b05465a37a9b423ab6945a5c
lillalysmasken3 <linuserklarsson@hotmail.com>
2020-04-04 10:10:32 +0200

    kommenterat koden

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,26 +1,26 @@
 #include <Arduino.h>
 
-#define UART Serial1
+#define UART Serial1 // bara ett namnbute så det är tydligare vilken serial vi använder.
 
 void setup(void)
 {
-    UART.begin(9600);
-    Serial.begin(9600);
-    delay(2000);
+    Serial.begin(9600); //Starta Serial AKA USB kommunikationen med en bitrate på 9600 bit/sekund.
+    UART.begin(9600);   //starta Serial1 AKA UART kommunikationen med en bitrate på 9600 bit/sekund.
+    delay(2000);        // chilla 2 sek tills de startat.
 }
 void loop(void)
 {
-    while (!UART.available())
-    {
+    while (!UART.available()) // Om det inte finns något i UART buffern...
+    {                         // gör inget
     }
-    char mychar = UART.read();
+    char mychar = UART.read(); //läs ett tecken från UART buffern.
 
-    Serial.print((char)mychar);
+    Serial.print((char)mychar); //printa tecknet.
 
-    mychar = toupper(mychar);
-    while (!UART.availableForWrite())
-    {
+    mychar = toupper(mychar);         //konvertera tecknet till stor bokstav.
+    while (!UART.availableForWrite()) // om inte UART buffern är tom...
+    {                                 //gör inget
     }
-    UART.write(mychar);
-    UART.flush();
+    UART.write(mychar); // annars skriv tecknet till buffern.
+    UART.flush();       //tvångsvänta tills den skickat iväg buffern. AKA tvinga den att skicka iväg buffern.
 }
--- teensy35/src/main.cpp
+++ teensy35/src/main.cpp
@@ -1,29 +1,28 @@
 #include <Arduino.h>
 
-#define UART Serial1
+#define UART Serial1 // bara ett namnbute så det är tydligare vilken serial vi använder.
 
 void setup(void)
 {
-    Serial.begin(9600);
-    UART.begin(9600);
-
-    delay(2000);
+    Serial.begin(9600); //Starta Serial AKA USB kommunikationen med en bitrate på 9600 bit/sekund.
+    UART.begin(9600);   //starta Serial1 AKA UART kommunikationen med en bitrate på 9600 bit/sekund.
+    delay(2000);        // chilla 2 sek tills de startat.
 }
 
 void loop(void)
 {
-    char mychar = 'a' + random(26);
+    char mychar = 'a' + random(26); //för en char som är lill a + ?? för att slumpa fram en liten bokstav
 
-    while (!UART.availableForWrite())
-    {
+    while (!UART.availableForWrite()) // om inte UART buffern är tom...
+    {                                 //gör inget
     }
-    UART.write(mychar);
-    UART.flush();
+    UART.write(mychar); // annars skriv tecknet till buffern.
+    UART.flush();       //tvångsvänta tills den skickat iväg buffern. AKA tvinga den att skicka iväg buffern.
 
-    while (!UART.available())
-    {
+    while (!UART.available()) // Om det inte finns något i UART buffern...
+    {                         // gör inget
     }
-    Serial.print((char)UART.read());
-    UART.clear();
+    Serial.print((char)UART.read()); //printa tecknet som läses från UART buffern.
+    UART.clear();                    //rensar buffern
     delay(1000);
 }
